a51376b585a3ce41fdb0449e638f1a90ed12d78a
40586 <40586@longroad.ac.uk>
2014-09-09 12:51:35 +0100

    noted code

---
+++ Basic program explained.py
@@ -0,0 +1,12 @@
+#Kieran Burnett
+#9-9-2014
+#Basic program
+
+# will print hello world
+print("hello world")
+#will create a varible of the persons name
+Name = input("what is your name?: ")
+#will print the varible
+print(Name)
+#will print the varible in a setnance using a placeholder
+print("Hello {0}! ".format(Name))
