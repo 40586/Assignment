d088be758d36eaf9446ab4ada7cba7ffebcf7259
40586 <40586@longroad.ac.uk>
2014-09-24 14:34:06 +0100

    homework 23-09-2014: forgot to put up on the day

---
+++ Math_unit_exercises_no3.py
@@ -0,0 +1,10 @@
+# Kieran Burnett
+# 24-09-2014
+# Math unit exercises 3
+
+import math
+
+answer_west = math.sin(60) * 20
+answer_east = math.cos(60) * 20
+
+print("the plane traveled {0} Km east and {1} Km north".format(answer_east, answer_west))
